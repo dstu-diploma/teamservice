d746fd7076f67bb0009ee4c7e0eb1bc21e0e0dba
Максим Захаренков <m.zaharenkov@mail.ru>
2025-04-18 17:33:06 +0300

    ✨ Общение с UserService

--- app/controllers/user/exceptions.py
+++ app/controllers/user/exceptions.py
@@ -4,3 +4,10 @@ from fastapi import HTTPException
 class UserDoesNotExistException(HTTPException):
     def __init__(self):
         super().__init__(status_code=400, detail="No user with such id!")
+
+
+class UserServiceError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=400, detail="User service is not available!"
+        )
