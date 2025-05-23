ee826d0700065295bd7b7d5a931014162049f1d8
maladesov <m.zaharenkov@mail.ru>
2025-05-24 00:36:41 +0300

    ✨ Получение uploads от UserService

--- app/ports/userservice/dto.py
+++ app/ports/userservice/dto.py
@@ -1,4 +1,20 @@
+from datetime import datetime
 from pydantic import BaseModel
+from enum import StrEnum
+
+
+class UserUploadsType(StrEnum):
+    Avatar = "avatar"
+    Cover = "cover"
+
+
+class UserUploadDto(BaseModel):
+    user_id: int
+    type: UserUploadsType
+    s3_key: str
+    content_type: str
+    uploaded_at: datetime
+    url: str | None
 
 
 class ExternalUserDto(BaseModel):
@@ -6,3 +22,4 @@ class ExternalUserDto(BaseModel):
     is_banned: bool
     formatted_name: str
     role: str
+    uploads: list[UserUploadDto] | None = None
